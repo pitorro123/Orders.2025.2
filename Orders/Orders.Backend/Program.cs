b36e51d97f130f9e6657d07d590ffaf553dbbbfa
ANDRES FELIPE MEJIA CORRALES <andresmejia246388@correo.itm.edu.co>
2025-08-10 21:18:13 -0500

    Add basic countries api controller

--- Orders/Orders.Backend/Program.cs
+++ Orders/Orders.Backend/Program.cs
@@ -4,11 +4,21 @@ using Orders.Backend.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(x =>x.UseSqlServer("name=LocalConnection"));
 
 var app = builder.Build();
+
+app.UseCors(x => x
+.AllowAnyMethod()
+.AllowAnyHeader()
+.SetIsOriginAllowed(origin => true)
+.AllowCredentials());
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
